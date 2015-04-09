771f9bb4237b4f82c535c78d4e7db99a87ac8441
baniu.yao <baniu.yao@gmail.com>
2015-04-09 11:48:51 +0800

    fix login bug

--- zapi.py
+++ zapi.py
@@ -11,19 +11,24 @@ try:
 except ImportError:
     import json
 
-import urllib2, subprocess, re, time
+import urllib2
+import subprocess
+
 
 class ZabbixAPIException(Exception):
     pass
+
 
 class ZabbixAPI(object):
     __auth = ''
     __id = 0
     _state = {}
+
     def __new__(cls, *args, **kw):
         if not cls._state.has_key(cls):
             cls._state[cls] = super(ZabbixAPI, cls).__new__(cls, *args, **kw)
         return cls._state[cls]
+
     def __init__(self, url, user, password):
         self.__url = url.rstrip('/') + '/api_jsonrpc.php'
         self.__user = user
@@ -32,43 +37,51 @@ class ZabbixAPI(object):
                 'DService', 'Event', 'Graph', 'Graphitem', 'History', 'Host', 'Hostgroup', 'Image', 'Item', 
                 'Maintenance', 'Map', 'Mediatype', 'Proxy', 'Screen', 'Script', 'Template', 'Trigger', 'User',
                 'Usergroup', 'Usermacro', 'Usermedia')
+
     def __getattr__(self, name):
         if name not in self._zabbix_api_object_list:
             raise ZabbixAPIException('No such API object: %s' % name)
         if not self.__dict__.has_key(name):
             self.__dict__[name] = ZabbixAPIObjectFactory(self, name)
         return self.__dict__[name]
+
     def login(self):
-        user_info = {'user'     : self.__user, 
-                     'password' : self.__password}
+        user_info = {'user': self.__user,
+                     'password': self.__password}
         obj = self.json_obj('user.login', user_info)
-        content = self.postRequest(obj)
+        try:
+            content = self.post_request(obj)
+        except urllib2.HTTPError:
+            raise ZabbixAPIException("Zabbix URL Error")
         try:
             self.__auth = content['result']
         except KeyError, e:
             e = content['error']['data']
             raise ZabbixAPIException(e)
-    def isLogin(self):
+
+    def is_login(self):
         return self.__auth != ''
+
     def __checkAuth__(self):
-        if not self.isLogin():
+        if not self.is_login():
             raise ZabbixAPIException("NOT logged in")
+
     def json_obj(self, method, params):
-        obj = { 'jsonrpc' : '2.0',
-                'method'  : method,
-                'params'  : params,
-                'auth'    : self.__auth,
-                'id'      : self.__id}
+        obj = {'jsonrpc': '2.0',
+                'method': method,
+                'params': params,
+                'id': self.__id}
+        if method != 'user.login':
+            obj['auth'] = self.__auth
         return json.dumps(obj)
-    def postRequest(self, json_obj):
-        #print 'Post: %s' % json_obj
-        headers = { 'Content-Type' : 'application/json-rpc',
-                    'User-Agent'   : 'python/zabbix_api'}
+
+    def post_request(self, json_obj):
+        headers = {'Content-Type': 'application/json-rpc',
+                   'User-Agent': 'python/zabbix_api'}
         req = urllib2.Request(self.__url, json_obj, headers)
         opener = urllib2.urlopen(req)
         content = json.loads(opener.read())
         self.__id += 1 
-        #print 'Receive: %s' % content
         return content
 
     '''
@@ -76,36 +89,39 @@ class ZabbixAPI(object):
     plus, the ip(computer run this script) must be put into the conf of agent.
     '''
     @staticmethod
-    def zabbixGet(ip, key):
-        zabbix_get = subprocess.Popen('/usr/local/zabbix/bin/zabbix_get -s %s -k %s' % (ip, key), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        result, err = zabbix_get.communicate()
+    def zabbix_get(ip, key):
+        zabbix_get_command = subprocess.Popen('/usr/local/zabbix/bin/zabbix_get -s %s -k %s' % (ip, key),
+                                              shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result, err = zabbix_get_command.communicate()
         if err:
             return 'ERROR'
         return result.strip()
 
-    def createObject(self, object_name, *args, **kwargs):
+    def create_object(self, object_name, *args, **kwargs):
         return object_name(self, *args, **kwargs)
 
-    def getHostByHostid(self, hostids):
+    def get_host_by_hostid(self, hostids):
         if not isinstance(hostids,list):
             hostids = [hostids]
-        return [dict['host'] for dict in self.host.get({'hostids':hostids,'output':'extend'})]
+        return [d['host'] for d in self.host.get({'hostids': hostids, 'output': 'extend'})]
 
 #################################################
 #    Decorate Method
 #################################################
 
-def checkAuth(func):
+
+def check_auth(func):
     def ret(self, *args):
         self.__checkAuth__()
         return func(self, args)
     return ret
 
-def postJson(method_name):
+
+def post_json(method_name):
     def decorator(func):
         def wrapper(self, params):
             try:
-                content = self.postRequest(self.json_obj(method_name, params))
+                content = self.post_request(self.json_obj(method_name, params))
                 return content['result']
             except KeyError, e:
                 e = content['error']['data']
@@ -113,10 +129,11 @@ def postJson(method_name):
         return wrapper
     return decorator
 
-def ZabbixAPIObjectMethod(func):
+
+def zabbix_api_object_method(func):
     def wrapper(self, method_name, params):
         try:
-            content = self.postRequest(self.json_obj(method_name, params))
+            content = self.post_request(self.json_obj(method_name, params))
             return content['result']
         except KeyError, e:
             e = content['error']['data']
@@ -127,26 +144,33 @@ def ZabbixAPIObjectMethod(func):
 #    Zabbix API Object (host, item...)    
 #################################################
 
+
 class ZabbixAPIObjectFactory(object):
     def __init__(self, zapi, object_name=''):
         self.__zapi = zapi
         self.__object_name = object_name
+
     def __checkAuth__(self):
         self.__zapi.__checkAuth__()
-    def postRequest(self, json_obj):
-        return self.__zapi.postRequest(json_obj)
+
+    def post_request(self, json_obj):
+        return self.__zapi.post_request(json_obj)
+
     def json_obj(self, method, param):
         return self.__zapi.json_obj(method, param)
+
     def __getattr__(self, method_name):
         def method(params):
-            return self.proxyMethod('%s.%s' % (self.__object_name,method_name), params)
+            return self.proxy_method('%s.%s' % (self.__object_name,method_name), params)
         return method
-    # 'find' method is a wrapper of get. Difference between 'get' and 'find' is that 'find' can create object you want while it dosn't exist
+
+    # 'find' method is a wrapper of get.
+    # Difference between 'get' and 'find' is that 'find' can create object you want while it doesn't exist
     def find(self, params, attr_name=None, to_create=False):
         filtered_list = []
-        result = self.proxyMethod('%s.get' % self.__object_name, {'output':'extend','filter': params})
+        result = self.proxy_method('%s.get' % self.__object_name, {'output': 'extend', 'filter': params})
         if to_create and len(result) == 0:
-            result = self.proxyMethod('%s.create' % self.__object_name, params)
+            result = self.proxy_method('%s.create' % self.__object_name, params)
             return result.values()[0]
         if attr_name is not None:
             for element in result:
@@ -156,7 +180,7 @@ class ZabbixAPIObjectFactory(object):
             return result
 
 
-    @ZabbixAPIObjectMethod
-    @checkAuth
-    def proxyMethod(self, method_name, params):
+    @zabbix_api_object_method
+    @check_auth
+    def proxy_method(self, method_name, params):
         pass
